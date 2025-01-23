7dc4c9c232c38278d0e07bbb75ab8f25224f8385
Zo3265 <zohaibgul03@gmail.com>
2025-01-23 10:55:08 +0000

    WIP Animations

--- Saloon Crawl/Assets/Scripts/Animations/MenuManager.cs
+++ Saloon Crawl/Assets/Scripts/Animations/MenuManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class SceneManagerScript : MonoBehaviour
+public class MenuManagerScript : MonoBehaviour
 {
     public void ChangeScene(string sceneName)
     {
